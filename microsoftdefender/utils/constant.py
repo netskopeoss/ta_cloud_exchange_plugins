f06394b65301b6cfdbc5cf6a9e2a0ddbcbc73310
Nijraj Gelani <nijraj.gelani@crestdatasys.com>
2022-10-25 17:57:34 +0530

    CTE Microsoft Defender for Endpoint 1.2.0 - Updated API endpoints

--- microsoftdefender/utils/constant.py
+++ microsoftdefender/utils/constant.py
@@ -1,19 +1,14 @@
-actions = ["unknown", "allow", "block", "alert"]
-
-target_products = ["Azure Sentinel", "Microsoft Defender ATP"]
-
-threat_types = [
-    "Botnet",
-    "C2",
-    "CryptoMining",
-    "Darknet",
-    "DDoS",
-    "MaliciousUrl",
-    "Malware",
-    "Phishing",
-    "Proxy",
-    "PUA",
-    "WatchList",
+actions = [
+    "Warn",
+    "Block",
+    "Audit",
+    "Alert",
+    "AlertAndBlock",
+    "BlockAndRemediate",
+    "Allowed"
 ]
 
-tlp_levels = ["unknown", "white", "green", "amber", "red"]
+allow_deletion = {
+    "Yes",
+    "No"
+}

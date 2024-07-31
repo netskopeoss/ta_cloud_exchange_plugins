50fd765a2ac3e5a6b9f01be2ebaeeff2b6191719
tanushree.kurup <tanushree.kurup@crestdatasys.com>
2024-07-31 14:18:51 +0530

    CTE Web Page IOC Scraper 1.1.0 - Renamed plugin from 'External Website' to 'Web Page IOC Scraper' and Added support to bifurcate the URL by types (Domain, IPv4, IPv6) starting from CE v5.0.1.

--- external_website/utils/externalwebsite_helper.py
+++ external_website/utils/externalwebsite_helper.py
@@ -29,7 +29,7 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-CTE External Website plugin helper module.
+CTE Web Page IOC Scraper Plugin helper module.
 """
 
 
@@ -44,18 +44,18 @@ from .externalwebsite_constants import (
     DEFAULT_WAIT_TIME,
     MAX_API_CALLS,
     MODULE_NAME,
-    PLUGIN_NAME
+    PLATFORM_NAME
 )
 
 
-class ExternalWebsitePluginException(Exception):
-    """ExternalWebsite plugin custom exception class."""
+class WebPageIOCScraperPluginException(Exception):
+    """WebPageIOCScraper plugin custom exception class."""
 
     pass
 
 
-class ExternalWebsitePluginHelper(object):
-    """ExternalWebsitePluginHelper class.
+class WebPageIOCScraperPluginHelper(object):
+    """WebPageIOCScraperPluginHelper class.
 
     Args:
         object (object): Object class.
@@ -68,7 +68,7 @@ class ExternalWebsitePluginHelper(object):
         plugin_name: str,
         plugin_version: str
     ):
-        """ExternalWebsitePluginHelper initializer.
+        """Web Page IOC Scraper Plugin Helper initializer.
 
         Args:
             logger (logger object): Logger object.
@@ -100,145 +100,282 @@ class ExternalWebsitePluginHelper(object):
         headers.update({"User-Agent": user_agent})
         return headers
 
-    def handle_error(self, response):
-        status_code = response.status_code
-
-        if status_code == 200:
-            return response.text
-
-        if status_code == 401:
-            err_msg = "Received exit code 401, Authentication Error."
-            self.logger.error(f"{self.log_prefix} {err_msg}.")
-            raise ExternalWebsitePluginException(err_msg)
-        elif status_code == 404:
-            err_msg = "Received exit code 404, Resource Not Found."
-            self.logger.error(f"{self.log_prefix} {err_msg}.")
-            raise ExternalWebsitePluginException(err_msg)
-        elif 400 <= status_code < 500:
-            err_msg = f"Received exit code {status_code}, HTTP Client Error."
-            self.logger.error(f"{self.log_prefix} {err_msg}.")
-            raise ExternalWebsitePluginException(err_msg)
-        elif 500 <= status_code < 600:
-            err_msg = f"Received exit code {status_code}, HTTP Server Error."
-            self.logger.error(f"{self.log_prefix} {err_msg}.")
-            raise ExternalWebsitePluginException(err_msg)
-        else:
-            err_msg = f"Received exit code {status_code}, HTTP Error."
-            self.logger.error(f"{self.log_prefix} {err_msg}.")
-            raise ExternalWebsitePluginException(err_msg)
-
     def api_helper(
         self,
-        url,
-        method,
-        params=None,
+        logger_msg: str,
+        url: str,
+        method: str = "GET",
+        params: Dict = {},
         data=None,
-        headers=None,
+        files=None,
+        headers: Dict = {},
         verify=True,
         proxies=None,
-        logger_msg=str
+        json=None,
+        is_handle_error_required=True,
+        is_validation=False,
+        regenerate_auth_token=True,
     ):
-        """API Helper perform API request to ThirdParty platform
+        """API Helper to perform API request on ThirdParty platform \
         and captures all the possible errors for requests.
 
         Args:
-            request (request): Requests object.
-            code is required?. Defaults to True.
+            logger_msg (str): Logger message.
+            url (str): API Endpoint.
+            method (str): Method for the endpoint.
+            params (Dict, optional): Request parameters dictionary.
+            Defaults to None.
+            data (Any,optional): Data to be sent to API. Defaults to None.
+            headers (Dict, optional): Headers for the request. Defaults to {}.
+            json (optional): Json payload for request. Defaults to None.
+            is_handle_error_required (bool, optional): Does the API helper
+            should handle the status codes. Defaults to True.
+            is_validation (bool, optional): Does this request coming from
+            validate method?. Defaults to False.
+            regenerate_auth_token (bool, optional): Is regenerating auth token
+            required? Defaults to True.
+            configuration (Dict): Configuration Dictionary.
+
 
         Returns:
-            dict: Response dictionary.
+            Response|Response JSON: Returns response json if
+            is_handle_error_required is True otherwise returns Response object.
         """
         try:
-            debuglog_msg = (f"{self.log_prefix} : API Request for "
-                            f"{logger_msg}. URL={url}.")
+            headers = self._add_user_agent(headers)
+
+            debug_log_msg = (
+                f"{self.log_prefix}: API Request for {logger_msg}."
+                f" Endpoint: {method} {url}"
+            )
             if params:
-                debuglog_msg += f", params={params}"
-            if data:
-                debuglog_msg += f", data={data}"
+                debug_log_msg += f", params: {params}."
 
-            self.logger.debug(debuglog_msg)
-
+            self.logger.debug(debug_log_msg)
             for retry_counter in range(MAX_API_CALLS):
                 response = requests.request(
                     url=url,
                     method=method,
                     params=params,
                     data=data,
-                    headers=self._add_user_agent(headers),
+                    headers=headers,
                     verify=verify,
                     proxies=proxies,
+                    json=json,
+                    files=files,
                 )
+                status_code = response.status_code
                 self.logger.debug(
-                    f"{self.log_prefix} : Received API Response while "
-                    f"{logger_msg} Method={method}, "
-                    f"Status Code={response.status_code}."
+                    f"{self.log_prefix}: Received API Response for "
+                    f"{logger_msg}. Status Code={status_code}."
                 )
-                if response.status_code >= 500 and response.status_code <= 600:
+                if status_code == 429 and not is_validation:
                     if retry_counter == MAX_API_CALLS - 1:
                         err_msg = (
-                            "Received exit code {}, HTTP Server Error."
-                            " Max retries for rate limit "
-                            "handler exceeded hence returning status"
-                            " code {}.".format(
-                                response.status_code,
-                                response.status_code,
-                            )
+                            f"Received exit code {status_code}, API rate "
+                            f"limit exceeded while {logger_msg}. Max "
+                            "retries for rate limit handler exceeded "
+                            "hence returning status code 429."
                         )
                         self.logger.error(
                             message=f"{self.log_prefix}: {err_msg}",
                             details=err_msg,
                         )
-                        raise ExternalWebsitePluginException(err_msg)
+                        raise WebPageIOCScraperPluginException(err_msg)
+                    retry_after = int(response.headers.get("Retry-After", 60))
+                    retry_after = int(retry_after)
+                    if retry_after > 300:
+                        err_msg = (
+                            "'Retry-After' value received from "
+                            f"response headers while {logger_msg} is greater "
+                            f" than 5 minutes hence "
+                            f"returning status code {status_code}."
+                        )
+                        self.logger.error(
+                            message=f"{self.log_prefix}: {err_msg}"
+                        )
+                        raise WebPageIOCScraperPluginException(err_msg)
+                    self.logger.error(
+                        message=(
+                            f"{self.log_prefix}: Received exit code 429, API rate limit"
+                            f" exceeded while {logger_msg}. Retrying after {retry_after} "
+                            f"seconds. {MAX_API_CALLS - 1 - retry_counter} retries remaining."
+                        )
+                    )
+                    time.sleep(retry_after)
+                elif (500 <= status_code <= 600) and not is_validation:
+                    if retry_counter == MAX_API_CALLS - 1:
+                        err_msg = (
+                            f"Received exit code {status_code}, while "
+                            f" {logger_msg}. Max retries for rate limit "
+                            "handler exceeded hence returning status"
+                            f" code {status_code}."
+                        )
+                        self.logger.error(
+                            message=f"{self.log_prefix}: {err_msg}",
+                            details=err_msg,
+                        )
+                        raise WebPageIOCScraperPluginException(err_msg)
 
                     self.logger.error(
                         message=(
-                            "{}: Received exit code {}, HTTP Server Error"
-                            " occurred. Retrying after {} "
-                            "seconds. {} retries remaining.".format(
-                                self.log_prefix,
-                                response.status_code,
-                                DEFAULT_WAIT_TIME,
-                                MAX_API_CALLS - 1 - retry_counter,
-                            )
-                        )
+                            f"{self.log_prefix}: Received exit code "
+                            f"{status_code}, HTTP server error occurred while "
+                            f"{logger_msg}. Retrying after {DEFAULT_WAIT_TIME}"
+                            f" seconds. {MAX_API_CALLS - 1 - retry_counter}"
+                            " retries remaining."
+                        ),
                     )
                     time.sleep(DEFAULT_WAIT_TIME)
-                else:
-                    return self.handle_error(response)
 
+                else:
+                    return (
+                        self.handle_error(response, logger_msg, is_validation)
+                        if is_handle_error_required
+                        else response
+                    )
+        except WebPageIOCScraperPluginException:
+            raise
         except requests.exceptions.ProxyError as error:
             err_msg = (
-                "Proxy error occurred. Verify the provided "
-                "proxy configuration."
+                f"Proxy error occurred while {logger_msg}. Verify the"
+                " proxy configuration provided."
             )
+            if is_validation:
+                err_msg = (
+                    "Proxy error occurred. Verify "
+                    "the proxy configuration provided."
+                )
+
             self.logger.error(
                 message=f"{self.log_prefix}: {err_msg} Error: {error}",
-                details=str(traceback.format_exc()),
+                details=traceback.format_exc(),
             )
-            raise ExternalWebsitePluginException(err_msg)
+            raise WebPageIOCScraperPluginException(err_msg)
         except requests.exceptions.ConnectionError as error:
-            err_msg = f"Unable to establish connection with {PLUGIN_NAME}."
+            err_msg = (
+                f"Unable to establish connection with {PLATFORM_NAME} "
+                f"platform while {logger_msg}. Proxy server or "
+                f"{PLATFORM_NAME} server is not reachable."
+            )
+            if is_validation:
+                err_msg = (
+                    f"Unable to establish connection with {PLATFORM_NAME} "
+                    f"platform. Proxy server or {PLATFORM_NAME}"
+                    " server is not reachable."
+                )
+
             self.logger.error(
                 message=f"{self.log_prefix}: {err_msg} Error: {error}",
-                details=str(traceback.format_exc()),
+                details=traceback.format_exc(),
             )
-            raise ExternalWebsitePluginException(err_msg)
+            raise WebPageIOCScraperPluginException(err_msg)
         except requests.HTTPError as err:
-            err_msg = "HTTP Error occurred."
+            err_msg = f"HTTP error occurred while {logger_msg}."
+            if is_validation:
+                err_msg = (
+                    "HTTP error occurred. Verify"
+                    " configuration parameters provided."
+                )
             self.logger.error(
                 message=f"{self.log_prefix}: {err_msg} Error: {err}",
-                details=str(traceback.format_exc()),
+                details=traceback.format_exc(),
             )
-            raise ExternalWebsitePluginException(err_msg)
-        except ExternalWebsitePluginException:
-            raise
+            raise WebPageIOCScraperPluginException(err_msg)
         except Exception as exp:
+            err_msg = f"Unexpected error occurred while {logger_msg}."
+            if is_validation:
+                err_msg = (
+                    "Unexpected error while performing "
+                    f"API call to {PLATFORM_NAME}."
+                )
+                self.logger.error(
+                    message=f"{self.log_prefix}: {err_msg} Error: {exp}",
+                    details=traceback.format_exc(),
+                )
+                raise WebPageIOCScraperPluginException(
+                    f"{err_msg} Check logs for more details."
+                )
+            self.logger.error(
+                message=f"{self.log_prefix}: {err_msg} Error: {exp}",
+                details=traceback.format_exc(),
+            )
+            raise WebPageIOCScraperPluginException(err_msg)
+
+    def handle_error(
+        self,
+        resp: requests.models.Response,
+        logger_msg: str,
+        is_validation: bool = False,
+    ):
+        """Handle the different HTTP response code.
+
+        Args:
+            resp (requests.models.Response): Response object
+            returned from API call.
+            logger_msg: logger message.
+            is_validation : API call from validation method or not
+        Returns:
+            dict: Returns the dictionary of response JSON
+            when the response code is 200.
+        Raises:
+            HTTPError: When the response code is not 200.
+        """
+        status_code = resp.status_code
+        validation_msg = "Validation error occurred, "
+        error_dict = {
+            400: "Received exit code 400, HTTP client error",
+            403: "Received exit code 403, Forbidden",
+            404: "Received exit code 404, Resource not found",
+        }
+        if is_validation:
+            error_dict = {
+                400: (
+                    "Received exit code 400, Bad Request, Verify the "
+                    " Website URL provided in the configuration parameters."
+                ),
+                403: (
+                    "Received exit code 403, Forbidden, Verify the"
+                    " Website URL provided in the configuration parameters."
+                ),
+                404: (
+                    "Received exit code 404, Resource not found, Verify "
+                    "Website URL provided in the configuration parameters."
+                ),
+            }
+
+        if status_code in [200, 201]:
+            return resp.text
+        elif status_code == 204:
+            return {}
+        elif status_code in error_dict:
+            err_msg = error_dict[status_code]
+            if is_validation:
+                log_err_msg = validation_msg + err_msg
+                self.logger.error(
+                    message=f"{self.log_prefix}: {log_err_msg}",
+                    details=f"API response: {resp.text}",
+                )
+                raise WebPageIOCScraperPluginException(err_msg)
+            else:
+                err_msg = err_msg + " while " + logger_msg + "."
+                self.logger.error(
+                    message=f"{self.log_prefix}: {err_msg}",
+                    details=f"API response: {resp.text}",
+                )
+                raise WebPageIOCScraperPluginException(err_msg)
+
+        else:
             err_msg = (
-                "Unexpected error occurred while requesting "
-                f"to {self.plugin_name}. Error: {exp}"
+                "HTTP Server Error"
+                if (status_code >= 500 and status_code <= 600)
+                else "HTTP Error"
             )
             self.logger.error(
-                message=f"{self.log_prefix}: {err_msg}",
-                details=str(traceback.format_exc()),
+                message=(
+                    f"{self.log_prefix}: Received exit code {status_code}, "
+                    f"{validation_msg+err_msg} while {logger_msg}."
+                ),
+                details=f"API response: {resp.text}",
             )
-            raise ExternalWebsitePluginException(err_msg)
+            raise WebPageIOCScraperPluginException(err_msg)

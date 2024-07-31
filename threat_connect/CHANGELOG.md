e9601f21a3edec308ee30064bf7d20050c6de5ec
tanushreekurup-crest <139557242+tanushreekurup-crest@users.noreply.github.com>
2024-07-31 22:13:46 +0530

    CTE ThreatConnect 1.2.0 - Added support to bifurcate the URL by types (Hostname, IPv4, IPv6 & URL) starting from CE v5.0.1. (#191)

--- threat_connect/CHANGELOG.md
+++ threat_connect/CHANGELOG.md
@@ -1,3 +1,7 @@
+# 1.2.0
+## Added
+- Added support to bifurcate the URL by types (Hostname, IPv4, IPv6 & URL) starting from CE v5.0.1.
+
 # 1.1.1
 ## Added
 - Added support to tag indicators based on the indicator type.
---
+++ threat_connect/utils/constants.py
@@ -0,0 +1,58 @@
+"""ThreatConnect Plugin constants."""
+
+from netskope.integrations.cte.models import SeverityType
+
+INDICATOR_TYPES = [
+    "File",
+    "URL",
+    "Host",
+    "Address"
+]
+
+RATING_TO_SEVERITY = {
+    0: SeverityType.UNKNOWN,
+    1: SeverityType.LOW,
+    2: SeverityType.LOW,
+    3: SeverityType.MEDIUM,
+    4: SeverityType.HIGH,
+    5: SeverityType.CRITICAL,
+}
+
+SEVERITY_TO_RATING = {
+    SeverityType.UNKNOWN: 0,
+    SeverityType.LOW: 1,
+    SeverityType.MEDIUM: 3,
+    SeverityType.HIGH: 4,
+    SeverityType.CRITICAL: 5,
+}
+
+LIMIT = 1000  # Maximum Response LIMIT at Time.
+PAGE_LIMIT = 100
+MAX_RETRY = 3
+TAG_NAME = "Netskope CE"
+MODULE_NAME = "CTE"
+PLUGIN_NAME = "ThreatConnect"
+PLUGIN_VERSION = "1.2.0"
+
+# Default Confidence value
+DEFAULT_CONFIDENCE = 50
+
+DEFAULT_WAIT_TIME = 60
+MAX_WAIT_TIME = 300
+
+
+THREAT_CONNECT_URLS = {
+    "owners": "/api/v3/security/owners",
+    "owners_mine": "/api/v2/owners/mine",
+    "indicators": "/api/v3/indicators",
+    "groups": "/api/v3/groups/",
+    "update_indicators": "/api/v3/indicators/{value}"
+}
+
+INTEGER_THRESHOLD = 4611686018427387904
+
+# Response Messages
+PUSH_INDICATOR_FAILURE = (
+    "This Indicator is contained on a system-wide exclusion list."
+)
+BIFURCATE_INDICATOR_TYPES = {"url", "domain", "ipv4", "ipv6", "hostname"}

6cdb9c0b2dca2d5ffa32f9d78ee4c6c44cec5aa4
Kaushal Patel <kaushal.patel@crestdata.ai>
2025-12-16 10:27:44 +0530

    Added CTE Infoblox v2.0.0 plugin.

---
+++ infoblox_tide/utils/helper.py
@@ -0,0 +1,488 @@
+"""
+BSD 3-Clause License
+
+Copyright (c) 2021, Netskope OSS
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+CTE Infoblox Plugin constants.
+"""
+
+import hashlib
+import json
+import time
+import traceback
+from typing import Dict, Tuple, List, Union
+
+import requests
+from netskope.common.utils import add_user_agent
+
+from .constants import (
+    MODULE_NAME,
+    PLATFORM_NAME,
+    DEFAULT_SLEEP_TIME,
+    MAX_API_CALLS,
+    RETRACTION,
+)
+
+
+class InfobloxPluginException(Exception):
+    """Infoblox plugin custom exception class."""
+
+    pass
+
+
+class InfobloxPluginHelper:
+    """Infoblox plugin helper module."""
+
+    def __init__(
+        self,
+        logger,
+        log_prefix: str,
+        plugin_name: str,
+        plugin_version: str,
+    ):
+        """Infoblox Plugin Helper initializer.
+
+        Args:
+            logger (logger object): Logger object.
+            log_prefix (str): log prefix.
+            plugin_name (str): Plugin name.
+            plugin_version (str): Plugin version.
+        """
+        self.log_prefix = log_prefix
+        self.logger = logger
+        self.plugin_name = plugin_name
+        self.plugin_version = plugin_version
+
+    def _add_user_agent(self, headers: Union[Dict, None] = None) -> Dict:
+        """Add User-Agent in the headers for third-party requests.
+
+        Args:
+            headers (Dict): Dictionary containing headers for any request.
+        Returns:
+            Dict: Dictionary after adding User-Agent.
+        """
+        if headers and "User-Agent" in headers:
+            return headers
+        headers = add_user_agent(header=headers)
+        ce_added_agent = headers.get("User-Agent", "netskope-ce")
+        user_agent = "{}-{}-{}-v{}".format(
+            ce_added_agent,
+            MODULE_NAME.lower(),
+            self.plugin_name.lower().replace(" ", "-"),
+            self.plugin_version,
+        )
+        headers.update({"User-Agent": user_agent})
+        return headers
+
+    def api_helper(
+        self,
+        logger_msg: str,
+        url: str,
+        method: str = "GET",
+        params: Dict = {},
+        data=None,
+        files=None,
+        headers: Dict = {},
+        json=None,
+        is_validation=False,
+        verify: bool = True,
+        proxies: Dict = {},
+        is_retraction: bool = False,
+        is_handle_error_required: bool = True,
+    ) -> Dict:
+        """
+        API helper to perform API request on ThirdParty platform and captures
+        all the possible errors for requests.
+
+        Args:
+            logger_msg (str): Logger message.
+            url (str): API Endpoint.
+            method (str): Method for the endpoint.
+            params (Dict): Request parameters dictionary.
+            data (Any,optional): Data to be sent to API. Defaults to None.
+            files (Any, optional): Files to be sent to API. Defaults to None.
+            headers (Dict, optional): Headers for the request. Defaults to {}.
+            json (optional): Json payload for request. Defaults to None.
+            is_validation (bool, optional): Does this request coming from
+                validate method?. Defaults to False.
+            verify (bool, optional): Verify SSL. Defaults to True.
+            proxies (Dict, optional): Proxies. Defaults to {}.
+            is_retraction (bool, optional): Is this called from the retraction.
+            is_handle_error_required (bool, optional): Is handling status code
+                is required?. Defaults to True.
+
+        Returns:
+            Dict: Response dictionary.
+        """
+        headers = self._add_user_agent(headers)
+        debug_log_msg = (
+            f"{self.log_prefix} : API Request for {logger_msg}."
+            f" Endpoint: {method} {url}"
+        )
+        if params:
+            debug_log_msg += f", params: {params}."
+        self.logger.debug(debug_log_msg)
+
+        if is_retraction and RETRACTION not in self.log_prefix:
+            self.log_prefix = self.log_prefix + f" [{RETRACTION}]"
+        try:
+            for retry_counter in range(MAX_API_CALLS):
+                response = requests.request(
+                    method=method,
+                    url=url,
+                    params=params,
+                    data=data,
+                    files=files,
+                    headers=headers,
+                    json=json,
+                    verify=verify,
+                    proxies=proxies,
+                )
+                status_code = response.status_code
+                self.logger.debug(
+                    f"{self.log_prefix}: Received API Response for "
+                    f"{logger_msg}. Status Code={status_code}."
+                )
+
+                if not is_validation and (
+                    status_code == 429 or status_code in range(500, 601)
+                ):
+                    if retry_counter == MAX_API_CALLS - 1:
+                        error_msg = (
+                            f"Received exit code {status_code}, While"
+                            f" {logger_msg}. Max retries limit "
+                            "exceeded hence returning status"
+                            f" code {status_code}."
+                        )
+                        self.logger.error(
+                            message=f"{self.log_prefix}: {error_msg}",
+                            details=f"API response: {response.text}",
+                        )
+                        raise InfobloxPluginException(error_msg)
+                    self.logger.error(
+                        message=(
+                            f"{self.log_prefix}: Received exit code"
+                            f" {status_code}, "
+                            f"While {logger_msg}. Retrying after "
+                            f"{DEFAULT_SLEEP_TIME} seconds. "
+                            f"{MAX_API_CALLS - 1 - retry_counter} "
+                            "retries left."
+                        ),
+                        details=f"API response: {response.text}",
+                    )
+                    time.sleep(DEFAULT_SLEEP_TIME)
+                else:
+                    return (
+                        self._handle_error(
+                            response=response,
+                            logger_msg=logger_msg,
+                            is_validation=is_validation,
+                        )
+                        if is_handle_error_required
+                        else response
+                    )
+        except InfobloxPluginException:
+            raise
+        except requests.exceptions.ReadTimeout as error:
+            err_msg = (
+                f"Read Timeout error occurred while {logger_msg} when trying"
+                f" to communicate with {PLATFORM_NAME} platform. "
+                f"Please verify if the platform UI is up and running."
+            )
+            if is_validation:
+                err_msg = (
+                    "Read Timeout error occurred when trying "
+                    f"to communicate with {PLATFORM_NAME} platform. "
+                    f"Please verify if the platform UI is up and running."
+                )
+
+            self.logger.error(
+                message=f"{self.log_prefix}: {err_msg} Error: {error}",
+                details=traceback.format_exc(),
+            )
+            raise InfobloxPluginException(err_msg)
+        except requests.exceptions.ProxyError as error:
+            err_msg = (
+                f"Proxy error occurred while {logger_msg} when trying "
+                f"to communicate with {PLATFORM_NAME} platform. "
+                "Verify the proxy configuration provided."
+            )
+            if is_validation:
+                err_msg = (
+                    "Proxy error occurred when trying "
+                    f"to communicate with {PLATFORM_NAME} platform. "
+                    "Verify the proxy configuration provided."
+                )
+
+            self.logger.error(
+                message=f"{self.log_prefix}: {err_msg} Error: {error}",
+                details=traceback.format_exc(),
+            )
+            raise InfobloxPluginException(err_msg)
+        except requests.exceptions.ConnectionError as e:
+            err_msg = (
+                f"Unable to establish connection with {PLATFORM_NAME} "
+                f"platform while {logger_msg}."
+                f" {PLATFORM_NAME} server is not reachable."
+            )
+            if is_validation:
+                err_msg = (
+                    f"Unable to establish connection with {PLATFORM_NAME} "
+                    f"platform. {PLATFORM_NAME} server is not reachable."
+                )
+
+            self.logger.error(
+                message=f"{self.log_prefix}: {err_msg} Error: {e}",
+                details=traceback.format_exc(),
+            )
+            raise InfobloxPluginException(err_msg)
+        except requests.HTTPError as e:
+            err_msg = f"HTTP error occurred while {logger_msg}."
+            if is_validation:
+                err_msg = (
+                    "HTTP error occurred. Verify configuration parameters"
+                    " provided."
+                )
+            self.logger.error(
+                message=f"{self.log_prefix}: {err_msg} Error: {e}",
+                details=traceback.format_exc(),
+            )
+            raise InfobloxPluginException(err_msg)
+        except Exception as e:
+            err_msg = f"Unexpected error occurred while {logger_msg}."
+            if is_validation:
+                err_msg = (
+                    f"Unexpected error while performing API call to "
+                    f"{PLATFORM_NAME}."
+                )
+                self.logger.error(
+                    message=f"{self.log_prefix}: {err_msg} Error: {e}",
+                    details=traceback.format_exc(),
+                )
+                raise InfobloxPluginException(
+                    f"{err_msg} Check logs for more details."
+                )
+            self.logger.error(
+                message=f"{self.log_prefix}: {err_msg} Error: {e}",
+                details=traceback.format_exc(),
+            )
+            raise InfobloxPluginException(err_msg)
+
+    def _handle_error(
+        self,
+        response: requests.models.Response,
+        logger_msg: str,
+        is_validation: bool = False,
+    ) -> Dict:
+        """
+        Handles the different HTTP response code.
+
+        Args:
+            response (requests.models.Response): Response object returned from
+                API call.
+            logger_msg (str): Logger message.
+            is_validation (bool, optional): API call from validation method or
+                not. Defaults to False.
+
+        Returns:
+            dict: Returns the dictionary of response JSON
+                when the response code is 200, 201 or 202.
+        """
+        status_code = response.status_code
+        validation_error_msg = "Validation error occurred, "
+        error_dict = {
+            400: "Received exit code 400 (Bad Request)",
+            403: "Received exit code 403 (Forbidden)",
+            401: "Received exit code 401 (Unauthorized access)",
+            404: "Received exit code 404 (Resource not found)",
+        }
+        if is_validation:
+            error_dict = {
+                400: (
+                    "Received exit code 400 (Bad Request), Verify the"
+                    " Infoblox API Base URL, and API Key provided in "
+                    " the configuration parameters."
+                ),
+                401: (
+                    "Received exit code 401 (Unauthorized), Verify "
+                    "API Key provided in the "
+                    "configuration parameters."
+                ),
+                403: (
+                    "Received exit code 403 (Forbidden), Verify API Key "
+                    "provided in the configuration parameters."
+                ),
+                404: (
+                    "Received exit code 404 (Resource not found), Verify "
+                    "Infoblox API Base URL provided in the configuration "
+                    "parameters."
+                ),
+            }
+        if response.status_code in [200, 201, 202]:
+            return self._parse_response(response, logger_msg, is_validation)
+        if response.status_code == 204:
+            return {}
+
+        if error_msg := error_dict.get(response.status_code):
+            if is_validation:
+                log_error_msg = validation_error_msg + error_msg
+                self.logger.error(
+                    message=f"{self.log_prefix}: {log_error_msg}",
+                    details=f"API response: {response.text}",
+                )
+                raise InfobloxPluginException(log_error_msg)
+            else:
+                err_msg = error_msg + " while " + logger_msg + "."
+                self.logger.error(
+                    message=f"{self.log_prefix}: {err_msg}",
+                    details=f"API response: {response.text}",
+                )
+                raise InfobloxPluginException(err_msg)
+        else:
+            err_msg = (
+                "HTTP Server Error"
+                if (status_code >= 500 and status_code <= 600)
+                else "HTTP Error"
+            )
+            if is_validation:
+                err_msg = validation_error_msg + err_msg
+            else:
+                err_msg += " while " + logger_msg + "."
+            self.logger.error(
+                message=(
+                    f"{self.log_prefix}: Received exit code {status_code}, "
+                    f"{err_msg} while {logger_msg}."
+                ),
+                details=f"API response: {response.text}",
+            )
+            raise InfobloxPluginException(err_msg)
+
+    def _parse_response(
+        self,
+        response: requests.models.Response,
+        logger_msg: str,
+        is_validation: bool = False,
+    ) -> Dict:
+        """
+        Parse Response will return JSON from response object.
+
+        Args:
+            response (response): Response object.
+            logger_msg (str): Logger message
+            is_validation: (bool): Check for validation
+
+        Returns:
+            Any: Response Json.
+        """
+        try:
+            return response.json()
+        except json.JSONDecodeError as err:
+            err_msg = (
+                f"Invalid JSON response received from API while {logger_msg}. "
+                f"Error: {str(err)}"
+            )
+            self.logger.error(
+                message=f"{self.log_prefix}: {err_msg}",
+                details=f"API response: {response.text}",
+            )
+            if is_validation:
+                err_msg = (
+                    "Verify Infoblox Base URL provided in the "
+                    "configuration parameters. Check logs for more details."
+                )
+            raise InfobloxPluginException(err_msg)
+        except Exception as exp:
+            err_msg = (
+                "Unexpected error occurred while parsing"
+                f" json response while {logger_msg}. Error: {exp}"
+            )
+            self.logger.error(
+                message=f"{self.log_prefix}: {err_msg}",
+                details=f"API response: {response.text}",
+            )
+            if is_validation:
+                err_msg = (
+                    "Unexpected validation error occurred, "
+                    "Verify Infoblox Base URL provided in the "
+                    "configuration parameters. Check logs for more details."
+                )
+            raise InfobloxPluginException(err_msg)
+
+    def get_configuration_parameters(
+        self, configuration: Dict
+    ) -> Tuple[
+        str, str, List[str], str, int, str, int, str, List[str], List[str]
+    ]:
+        """
+        Get configuration parameters from the configuration dictionary.
+
+        Args:
+            configuration (Dict): Dictionary containing the configuration
+                parameters.
+
+        Returns:
+            Tuple[str, str, List[str], str, int, str, int, str, List[str],
+                List[str]]: Tuple containing the configuration parameters.
+        """
+        return (
+            configuration.get("base_url").strip().strip("/"),
+            configuration.get("api_key"),
+            configuration.get("type"),
+            configuration.get("is_pull_required", ""),
+            configuration.get("retraction_interval"),
+            configuration.get("enable_tagging", ""),
+            configuration.get("initial_pull_range"),
+            configuration.get("data_profile", ""),
+            configuration.get("indicator_source_page"),
+            configuration.get("soc_insight_ioc_action_type", [])
+        )
+
+    def get_auth_headers(self, api_key: str) -> Dict:
+        """
+        Get the authentication headers for the Infoblox plugin.
+
+        Args:
+            api_key (str): The API key for the Infoblox plugin.
+
+        Returns:
+            Dict: A dictionary containing the authentication headers.
+        """
+        return {"Authorization": f"Token token={api_key}"}
+
+    def generate_hash(self, value: str):
+        """
+        Generate a SHA-256 hash for the given string value.
+
+        Args:
+            value (str): The input string to hash.
+
+        Returns:
+            str: The hexadecimal SHA-256 hash of the input value.
+        """
+        return hashlib.sha256(value.encode()).hexdigest()

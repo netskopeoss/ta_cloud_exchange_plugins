b629a2f6c30f473b8a275e7c477d812e9598171d
ambrish2002 <ambrish.shukla@crestdata.ai>
2026-09-21 19:49:01 +0530

    Added CFC Linux file share v1.1.1 plugin (#445)

--- linux_file_share_cfc/main.py
+++ linux_file_share_cfc/main.py
@@ -470,12 +470,12 @@ class LinuxFileShareCFCPlugin(PluginBase):
             )
 
             if not metadata and not success:
-                self.logger.info(
+                self.logger.error(
                     f"{self.log_prefix}: No {pull_target} were pulled."
                     f"{error_suffix}"
                 )
             elif partial_errors:
-                self.logger.info(
+                self.logger.error(
                     f"{self.log_prefix}: {error_suffix}"
                 )
             else:
